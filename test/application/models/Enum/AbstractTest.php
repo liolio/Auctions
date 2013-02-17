909a243cafd5142768824810dd2785c1c69ecac2
Damian <damianlew1@gmail.com>
2013-02-17 23:06:49 +0100

    [NONISSUE]Refactoring enum abstrat test

---
+++ test/application/models/Enum/AbstractTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @class AbstractTest
+ */
+class Enum_AbstractTest extends TestCase_NoDatabase
+{
+    /**
+     * @test
+     * @dataProvider dataProvider
+     */
+    public function hasEnum($enum, $result)
+    {
+        $this->assertEquals($result, Enum_AbstractMock::hasEnum($enum));
+    }
+    
+    public function dataProvider()
+    {
+        return array(
+            array(Enum_AbstractMock::TEST, true),
+            array('super', true),
+            array('non_existing', false),
+            array(null, false),
+            array(false, false),
+            array(array(), false),
+        );
+    }
+}
+
+class Enum_AbstractMock extends Enum_Abstract
+{
+    const TEST = 'test';
+    
+    const SUPER = 'super';
+    
+    private function __construct()
+    {
+    }
+}
