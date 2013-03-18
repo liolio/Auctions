c05ff28c3aab98089de8da44d8703318fe728f7a
Damian <damianlew1@gmail.com>
2013-03-18 22:07:54 +0100

    [24/27]Add address

--- htdocs/application/modules/auctions/controllers/AddressController.php
+++ htdocs/application/modules/auctions/controllers/AddressController.php
@@ -14,4 +14,73 @@ class Auctions_AddressController extends Zend_Controller_Action
     {
         $this->view->addressList = Auth_User::getInstance()->getUser()->Addresses;
     }
+    
+    public function addAction()
+    {
+        $this->view->addForm = new Auctions_Form_Address_Add();
+    }
+    
+    public function processAddFormAction()
+    {
+        $request = $this->getRequest();
+
+        if (!$request->isPost())
+            return $this->_helper->redirector('add');
+        
+        $form = new Auctions_Form_Address_Add();
+        if (!$form->isValid($request->getPost()))
+        {
+            $this->view->addForm = $form;
+            return $this->render('add');
+        }
+        
+        try {
+            Doctrine_Manager::connection()->beginTransaction();
+            Address_Factory::create(Auth_User::getInstance()->getUser(), array(
+                FieldIdEnum::ADDRESS_NAME           =>  $form->getValue(FieldIdEnum::ADDRESS_NAME),
+                FieldIdEnum::ADDRESS_SURNAME        =>  $form->getValue(FieldIdEnum::ADDRESS_SURNAME),
+                FieldIdEnum::ADDRESS_STREET         =>  $form->getValue(FieldIdEnum::ADDRESS_STREET),
+                FieldIdEnum::ADDRESS_POSTAL_CODE    =>  $form->getValue(FieldIdEnum::ADDRESS_POSTAL_CODE),
+                FieldIdEnum::ADDRESS_CITY           =>  $form->getValue(FieldIdEnum::ADDRESS_CITY),
+                FieldIdEnum::ADDRESS_COUNTRY        =>  $form->getValue(FieldIdEnum::ADDRESS_COUNTRY),
+                FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  $form->getValue(FieldIdEnum::ADDRESS_PHONE_NUMBER),
+                FieldIdEnum::ADDRESS_PROVINCE       =>  $form->getValue(FieldIdEnum::ADDRESS_PROVINCE)
+            ))->save();
+            
+            Doctrine_Manager::connection()->commit();
+        }
+        catch (Exception $ex)
+        {
+            Doctrine_Manager::connection()->rollback();
+            Log_Factory::create($ex, Zend_Log::CRIT);
+            $this->view->registrationForm = $form;
+            $form->setDescription('Failure!');
+            return $this->render('add');
+        }
+        
+        $this->_helper->redirector('show-list', 'address');
+    }
+    
+    public function deleteAction()
+    {
+        if (Auth_User::getInstance()->getUser()->Addresses->count() === 1) 
+        {
+            $this->view->addressList = Auth_User::getInstance()->getUser()->Addresses;
+            $this->view->message = Helper::getTranslator()->translate("validation_message-address_cannot_remove_last_address");
+            return $this->render("showList");
+        }
+        
+        try {
+            Doctrine_Manager::connection()->beginTransaction();
+            AddressTable::getInstance()->findOneBy('id', $this->getRequest()->getParam(FieldIdEnum::ADDRESS_ID))->delete();
+            Doctrine_Manager::connection()->commit();
+        }
+        catch (Exception $ex)
+        {
+            Doctrine_Manager::connection()->rollback();
+            Log_Factory::create($ex, Zend_Log::CRIT);
+        }
+        
+        $this->_helper->redirector('show-list', 'address');
+    }
 }
---
+++ test/application/modules/forms/Address/AddTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @class Auctions_Form_Address_AddTest
+ */
+class Auctions_Form_Address_AddTest extends TestCase_NoDatabase
+{
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->_form = new Auctions_Form_Address_Add();
+    }
+    
+    /**
+     * @test
+     */
+    public function isValidWithValidValues()
+    {
+        $this->assertTrue($this->_form->isValid(array(
+            FieldIdEnum::ADDRESS_NAME           =>  'name',
+            FieldIdEnum::ADDRESS_SURNAME        =>  'sur-name',
+            FieldIdEnum::ADDRESS_STREET         =>  'street 1/1',
+            FieldIdEnum::ADDRESS_POSTAL_CODE    =>  'postal 123',
+            FieldIdEnum::ADDRESS_CITY           =>  'city',
+            FieldIdEnum::ADDRESS_PROVINCE       =>  'province',
+            FieldIdEnum::ADDRESS_COUNTRY        =>  'country',
+            FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  '123456890',
+        )));
+    }
+    
+    /**
+     * @test
+     * @dataProvider invalidValuesProvider
+     */
+    public function isValidWithInvalidValues(array $data, array $errors)
+    {
+        $this->assertFalse($this->_form->isValid($data));
+        
+        $this->assertEquals($errors, $this->_form->getErrors());
+    }
+    
+    public function invalidValuesProvider()
+    {
+        return array(
+            //empty
+            array(
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  '',
+                    FieldIdEnum::ADDRESS_SURNAME        =>  '',
+                    FieldIdEnum::ADDRESS_STREET         =>  '',
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  '',
+                    FieldIdEnum::ADDRESS_CITY           =>  '',
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  '',
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  '',
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  '',
+                ),
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_SURNAME        =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_STREET         =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_CITY           =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  array(Zend_Validate_NotEmpty::IS_EMPTY),
+                    ParamIdEnum::SUBMIT_BUTTON          =>  array()
+                )
+            ),
+            //too long
+            array(
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_SURNAME        =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_STREET         =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_CITY           =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  str_repeat('a', 101),
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  str_repeat('a', 101),
+                ),
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_SURNAME        =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_STREET         =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_CITY           =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  array(Zend_Validate_StringLength::TOO_LONG),
+                    ParamIdEnum::SUBMIT_BUTTON          =>  array()
+                )
+            ),
+            //invalid regex or alnum or alpha
+            array(
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  'name132',
+                    FieldIdEnum::ADDRESS_SURNAME        =>  'sur-name123',
+                    FieldIdEnum::ADDRESS_STREET         =>  'street 1!',
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  'postal+123',
+                    FieldIdEnum::ADDRESS_CITY           =>  'city1',
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  'province1',
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  'country1',
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  '123456890p',
+                ),
+                array(
+                    FieldIdEnum::ADDRESS_NAME           =>  array(Zend_Validate_Alpha::NOT_ALPHA),
+                    FieldIdEnum::ADDRESS_SURNAME        =>  array(Validate_Address_SurnameRegex::NOT_MATCH),
+                    FieldIdEnum::ADDRESS_STREET         =>  array(Validate_Address_StreetRegex::NOT_MATCH),
+                    FieldIdEnum::ADDRESS_POSTAL_CODE    =>  array(Validate_Address_PostalCodeRegex::NOT_MATCH),
+                    FieldIdEnum::ADDRESS_CITY           =>  array(Zend_Validate_Alpha::NOT_ALPHA),
+                    FieldIdEnum::ADDRESS_PROVINCE       =>  array(Zend_Validate_Alpha::NOT_ALPHA),
+                    FieldIdEnum::ADDRESS_COUNTRY        =>  array(Zend_Validate_Alpha::NOT_ALPHA),
+                    FieldIdEnum::ADDRESS_PHONE_NUMBER   =>  array(Validate_Address_PhoneNumberRegex::NOT_MATCH),
+                    ParamIdEnum::SUBMIT_BUTTON          =>  array()
+                )
+            )
+        );
+    }
+}
