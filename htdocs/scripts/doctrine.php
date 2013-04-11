93bc52b8cdc4cd0d7efb4dd141536781b62ed5b6
Damian <damianlew1@gmail.com>
2013-04-12 01:08:21 +0200

    [92] Setup production machine

--- htdocs/scripts/doctrine.php
+++ htdocs/scripts/doctrine.php
@@ -4,7 +4,7 @@ defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', dirname(__FILE__) . '/../application');
 
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
